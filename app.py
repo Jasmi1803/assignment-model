243f00a537f4ebb41af6115c87937a6e1fd823d0
Jasmi1803 <142863652+Jasmi1803@users.noreply.github.com>
2023-09-06 14:23:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def reserve(feature1,feature2,feature3,feature4,feature5,feature6,feature7,featu
 def main():
    st.title("REservation Cancellation")
 
-   feature1 = st.number_input("Feature 1", value=0.0)
+   feature1 = st.number_input("No Of Adults", value=0.0)
    feature2 = st.number_input("Feature 2", value=0.0)
    feature3 = st.number_input("Feature 3", value=0.0)
    feature4 = st.number_input("Feature 4", value=0.0)
